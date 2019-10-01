e22c1416a619ae1a48130eddad119331c43fcf3d
youvovk <wer3wollf@gmail.com>
2019-10-02 00:53:34 +0300

    fix media

--- build/precache-manifest.8febb972808271fda25e6c5a7716c7fc.js
+++ build/precache-manifest.8febb972808271fda25e6c5a7716c7fc.js
@@ -1,18 +1,18 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "1e25fb2ef2f99d0ce459a4f321c2e8b2",
+    "revision": "f6c9c90a3e528e0138e7f3084239d2c1",
     "url": "/react_redux-list-of-todos/index.html"
   },
   {
-    "revision": "133a3b0321c0274717d8",
-    "url": "/react_redux-list-of-todos/static/css/main.5425e027.chunk.css"
+    "revision": "04d693c074ee7080b1bd",
+    "url": "/react_redux-list-of-todos/static/css/main.674607d1.chunk.css"
   },
   {
     "revision": "054a163bf78bd73b042c",
     "url": "/react_redux-list-of-todos/static/js/2.8dedda5f.chunk.js"
   },
   {
-    "revision": "133a3b0321c0274717d8",
+    "revision": "04d693c074ee7080b1bd",
     "url": "/react_redux-list-of-todos/static/js/main.da990e74.chunk.js"
   },
   {
