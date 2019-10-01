4a0e4ee0d9f9fab6209d0435b0164cde470baa8b
youvovk <wer3wollf@gmail.com>
2019-10-01 21:01:11 +0300

    Updates

---
+++ precache-manifest.1ed3281f7230afabfacc014dec443db3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bec467aca38fcf9c2e76a7420f57b962",
+    "url": "/react_redux-list-of-todos/index.html"
+  },
+  {
+    "revision": "8a47298db19b9606f899",
+    "url": "/react_redux-list-of-todos/static/css/main.28685a84.chunk.css"
+  },
+  {
+    "revision": "00d6aa5796783c9219cf",
+    "url": "/react_redux-list-of-todos/static/js/2.b101d3db.chunk.js"
+  },
+  {
+    "revision": "8a47298db19b9606f899",
+    "url": "/react_redux-list-of-todos/static/js/main.fecdfc89.chunk.js"
+  },
+  {
+    "revision": "37dc44999646af065e8e",
+    "url": "/react_redux-list-of-todos/static/js/runtime~main.054cc81e.js"
+  },
+  {
+    "revision": "940184e8b50b6848cc461258463890a8",
+    "url": "/react_redux-list-of-todos/static/media/FIRST.940184e8.png"
+  },
+  {
+    "revision": "2a3e6b596c9d9d27f1795d658dfe1c43",
+    "url": "/react_redux-list-of-todos/static/media/screen1.2a3e6b59.png"
+  }
+]);
